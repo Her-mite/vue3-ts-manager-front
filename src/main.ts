b5a202127fed88f585a19a0711c52714345de927
hujq <akjdfjjf22@qq.com>
2022-03-17 14:59:09 +0800

    完成登录页面设计

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import ElementPlus from "element-plus";
+import * as ELIcons from "@element-plus/icons-vue";
+
 import "element-plus/dist/index.css";
 
-createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
+const app = createApp(App);
+
+app.use(store).use(router).use(ElementPlus);
+// 引入elementIcon
+Object.keys(ELIcons).forEach((key) => {
+  app.component(key, ELIcons[key as keyof typeof ELIcons]);
+});
+app.mount("#app");
--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="background-img">
+    <!-- 登录表单 -->
     <div class="ms-login">
       <div class="login-title">后台管理系统</div>
       <el-form
@@ -8,12 +9,35 @@
         :rules="rules"
         :model="loginFormData"
         style="max-width: 460px"
+        hide-required-asterisk
       >
-        <el-form-item label="用户名" prop="name">
-          <el-input v-model="loginFormData.name" />
+        <el-form-item prop="name">
+          <el-input v-model="loginFormData.name" placeholder="请输入用户名">
+            <template #prepend>
+              <el-icon><avatar /></el-icon>
+            </template>
+          </el-input>
         </el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input v-model="loginFormData.password" type="password" />
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginFormData.password"
+            type="password"
+            placeholder="请输入密码"
+          >
+            <template #prepend>
+              <el-icon><lock /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <div class="button-item">
+            <el-button type="primary" @click="submitForm(ruleFormRef)"
+              >登录</el-button
+            >
+            <el-button class="register-button" @click="resetForm(ruleFormRef)"
+              >注册</el-button
+            >
+          </div>
         </el-form-item>
       </el-form>
     </div>
@@ -67,19 +91,6 @@ export default defineComponent({
         // 自定义格式校验
         { validator: validatePassword, trigger: "blur" },
       ],
-      // password: [
-      //   {
-      //     required: true,
-      //     message: "Please input password",
-      //     trigger: "blur",
-      //   },
-      //   {
-      //     min: 6,
-      //     max: 20,
-      //     message: "Length should be 6 to 20",
-      //     trigger: "blur",
-      //   },
-      // ],
     });
     return { loginFormData, rules };
   },
@@ -94,9 +105,68 @@ export default defineComponent({
   width: 100%;
   height: 100%;
   z-index: -10;
-  background-image: url(@/assets/img/login-bg.jpg);
+  background-image: url(@/assets/img/login-img.jpg);
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+}
+/* 登录框 */
+.ms-login {
+  background: #00000088;
+  position: absolute;
+  width: 350px;
+  margin: -150px 0 0 -175px;
+  left: 50%;
+  top: 50%;
+  border-radius: 15px;
+}
+/* 标题 */
+.login-title {
+  width: 100%;
+  line-height: 50px;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 500;
+  color: #fff;
+  border-bottom: 1.5px solid #ddd;
+}
+/* 表单 */
+.el-form--label-right {
+  padding: 20px 40px 0 0;
+}
+
+.el-form-item__content {
+  margin-left: 50px !important;
+}
+
+/* 表单项 */
+.el-form-item {
+  margin-bottom: 30px !important;
+}
+
+/* 表单 标签 */
+/* .el-form-item__label {
+  color: white !important;
+} */
+/* 表单前缀图标样式 */
+.el-input-group__prepend {
+  padding: 0 15px !important;
+  background-color: #ffffff00 !important;
+}
+/* 输入框 */
+.ms-login .el-input__inner {
+  background: none;
+  border: none;
+}
+/* 注册按钮 */
+.register-button {
+  margin-left: 45px !important;
+}
+.button-item {
+  width: 300px;
+  flex-direction: row;
+  flex: 1;
+  justify-content: space-around;
+  margin-bottom: -10px;
 }
 </style>
