97d6b93eec49867992ae5ad17d344d42854def5f
hujq <akjdfjjf22@qq.com>
2022-04-05 15:34:19 +0800

    新增tag位置布局

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -3,10 +3,17 @@
     <!-- 头部 -->
     <HomeHeader />
 
-    <!-- menu -->
-    <HomeSider/>
+    <!-- 侧边菜单栏 -->
+    <HomeSider />
 
-    <router-view />
+    <div class="content-box">
+      <home-tag></home-tag>
+      <div class="content">
+      <router-view />
+
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -14,6 +21,7 @@
 import { defineComponent } from 'vue';
 import HomeHeader from './HomeHeader.vue';
 import HomeSider from './HomeSider.vue';
+import HomeTag from './HomeTag.vue';
 
 export default defineComponent({
   setup() {
@@ -21,7 +29,28 @@ export default defineComponent({
   },
   components: {
     HomeHeader,
-    HomeSider
+    HomeSider,
+    HomeTag,
   },
 });
 </script>
+
+<style>
+.content-box {
+    position: absolute;
+    left: 220px;
+    right: 0;
+    top: 55px;
+    bottom: 0;
+    padding-bottom: 30px;
+    -webkit-transition: left .3s ease-in-out;
+    transition: left .3s ease-in-out;
+}
+
+.content {
+    width: auto;
+    height: 100%;
+    padding: 10px;
+    box-sizing: border-box;
+}
+</style>
